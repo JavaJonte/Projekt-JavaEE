4bfa2774e5f8177e17e59d38546da23e79edb5f9
Jonathan Wikman <73289628+JavaJonte@users.noreply.github.com>
2021-10-19 23:58:42 +0200

    added temporary login sulution..

--- src/main/java/com/example/users/controller/UserController.java
+++ src/main/java/com/example/users/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.users.controller;
 
+import com.example.users.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class UserController {
 
+    @Autowired
+    UserService repo;
 
-    @RequestMapping("/")
-    public String getIndex(@RequestParam(value = "id", required = false) Integer id, Model model) {
 
-        System.out.println("SER VI DETTA I LOGGEN?");
-        model.addAttribute("id", id);
-        return "index";
+    @RequestMapping("/myAccount")
+    public String getIndex(Integer id, Model model) {
+
+        System.out.println("INDEX SYNS I LOGGEN?"); // VISAS I KONSOLL VARJE GÅNG SIDAN UPPDATERAS
+        System.out.println(repo.getAllUsers()); // KÖR EN QUERY MOT DATABASEN OCH LOGGAR...
+        return "index";                         // ...DET, PLUS LISTAN PÅ ALLA ANVÄNDARE I KONSOLLEN
+    }
+
+    @RequestMapping("/user")
+    public String getUser(Integer id, Model model) {
+        System.out.println("GET USER I LOGGEN");
+
+
+        return "user";
     }
 
 
