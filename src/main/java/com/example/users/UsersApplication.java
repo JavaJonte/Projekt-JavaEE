d781a0fce7fb1335fa4c49161a347ca4b8a8f2f2
Jonathan Wikman <73289628+JavaJonte@users.noreply.github.com>
2021-10-19 12:09:26 +0200

    Mob prog, skapat och testat create med JPA och DB

--- src/main/java/com/example/users/UsersApplication.java
+++ src/main/java/com/example/users/UsersApplication.java
@@ -1,21 +1,34 @@
 package com.example.users;
 
 import com.example.users.model.User;
+import com.example.users.service.UserService;
 import com.example.users.service.UserServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class UsersApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UsersApplication.class, args);
+       ConfigurableApplicationContext context = SpringApplication.run(UsersApplication.class, args);
+       UserService userService = context.getBean(UserService.class);
+
+        User test = new User(1,"Hej", "Då", "email", "HDÅ","KOD");
+
+       userService.createUser(test);
+
+        System.out.println("DETTA SKRIVS EFTER TILLAGT");
+
+
+
+
+
     }
 
 
-    User test = new User(1,"Hej", "Då", "email", "HDÅ","KOD");
 
-    UserServiceImpl hej = new UserServiceImpl();
 
 
 }
+
