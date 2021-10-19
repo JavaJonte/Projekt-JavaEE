1c8cd89e5bb49aa21c9deacce2840b063cad0195
Jonathan Wikman <73289628+JavaJonte@users.noreply.github.com>
2021-10-19 11:57:01 +0200

    mob prog commit

--- src/main/java/com/example/users/UsersApplication.java
+++ src/main/java/com/example/users/UsersApplication.java
@@ -1,5 +1,7 @@
 package com.example.users;
 
+import com.example.users.model.User;
+import com.example.users.service.UserServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +11,11 @@ public class UsersApplication {
     public static void main(String[] args) {
         SpringApplication.run(UsersApplication.class, args);
     }
+
+
+    User test = new User(1,"Hej", "Då", "email", "HDÅ","KOD");
+
+    UserServiceImpl hej = new UserServiceImpl();
+
 
 }
